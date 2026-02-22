782d7c85229b90f62c70b8f9c56ba94b202493f3
Bobby_Pamarthi <84150177+BobbyPamarthi@users.noreply.github.com>
2026-02-22 12:58:50 +0530

    Update greeting message in app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello from Jenkins CI/CD Docker pipeline 🚀\n');
+  res.end('Hello Bob Jenkins CI/CD Docker pipeline is working🚀\n');
 });
 
 server.listen(PORT, () => {
